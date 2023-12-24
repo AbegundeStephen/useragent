18265abc5df0c8ab7017d471a01c7623925f3cd1
AbegundeStephen <timilehin18@gmail.com>
2023-12-24 13:13:48 +0100

    removed shortid package

--- server/index.js
+++ server/index.js
@@ -45,7 +45,7 @@ app.use(error)
  const PORT = process.env.PORT
 
  mongoose
- .connect("mongodb://localhost:27017/user-agent", 
+ .connect(process.env.MONGO_URL, 
     {
         useNewUrlParser: true, 
         useUnifiedTopology: true
