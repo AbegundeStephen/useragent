5679b385bcd0f9c0fb2b4b678e6b96bc49d16bdf
AbegundeStephen <timilehin18@gmail.com>
2023-12-24 16:41:23 +0100

    deleted multiple response.json

--- server/authMiddleware/error.js
+++ server/authMiddleware/error.js
@@ -3,6 +3,7 @@ const error = (err, req, res, next) => {
     console.log("statuscode",statusCode)
   
     res.status(statusCode).json({message:err.message,stack: process.env.NODE_ENV === "development" || "production" ? err.stack : null});
+    next()
   };
 
   export default error
